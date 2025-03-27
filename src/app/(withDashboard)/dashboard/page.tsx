96707855cadb4932a706b5c4945f24feaeb96804
Abadat Hossain <abadat26@gmail.com>
2025-03-27 11:11:32 +0600

    login authentication done

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,8 +1,28 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
-    <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
-    </div>
+    <>
+      {session?.user && (
+        <>
+          <div>
+            <h1 className="text-4xl text-center mt-10">
+              Welcome To Dashboard Page{session?.user?.name}
+            </h1>
+            <Image
+              src={session?.user?.image}
+              alt="image"
+              width={100}
+              height={100}
+            />
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
